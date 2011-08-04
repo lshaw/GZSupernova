a8801f155f3ffca97249c7248615f11fc6bec2a1
Liam Shaw <liam.shaw@astro.ox.ac.uk>
2011-08-04 12:10:18 +0100

    changes to parsing method from XML to JSON

--- GZJsonParser.m
+++ GZJsonParser.m
@@ -13,6 +13,9 @@
 
 @implementation GZJsonSimpleElementFinder
 
+@synthesize targetTag;
+@synthesize tagValue;
+
 -(id) initWithData:(NSData *)data
 {
     self=[super init];
@@ -37,49 +40,59 @@
     if (!_data) return nil;
     //self.tagValue=nil;
     self.targetTag=tag;
-    inTag=NO;
+    inTag=NO; // not sure what's going on with these bits
 	
-	//think this is the correct parsing method...
+	//think this is the correct parsing method using SBJsonParser
 
     SBJsonParser * parser = [[SBJsonParser alloc] initWithData:_data];
-	NSString * return_value = [NSString stringWithString:[parser objectWithString:self.targetTag] error:nil]; 	//[parser parse];
+	NSError *error = nil;
+	NSString * return_value = [NSString stringWithString:[parser objectWithString:self.targetTag] error:&error]; 
 	[parser release];
 	
     return return_value;
 }
-- (void)parser:(SBJsonParser *)parser didStartElement:(NSString *)elementName 
-  namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName 
-	attributes:(NSDictionary *)attributeDict 
+
+// for starting to parse
+- (void) parser:(SBJsonParser *)parser 
+didStartElement:(NSString *)elementName 
+   namespaceURI:(NSString *)namespaceURI 
+  qualifiedName:(NSString *)qName 
+	 attributes:(NSDictionary *)attributeDict 
 {
-	if ( [elementName isEqualToString:targetTag]) 
+	// Check if the element is equal to the targetTag, and if it is create a mutable string
+	// for storing the tagValue
+	if ([elementName isEqualToString:targetTag]) 
 	{
         inTag=YES;
-        self.tagValue = [NSMutableString stringWithCapacity:50];
+        self.tagValue = [NSMutableString stringWithCapacity:50]; 
     }
 }
 
 - (void)parser:(SBJsonParser *)parser foundCharacters:(NSString *)string
 {
-    if (!inTag) return;
-    [self.tagValue appendString:string];
+    if (!inTag) return; // if the flag is NO, then character value is not appending to tagValue
+    [self.tagValue appendString:string]; // otherwise, append the character to the tagValue
     
 }
 
-- (void)parser:(SBJsonParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+// for ending parsing
+- (void)parser:(SBJsonParser *)parser 
+ didEndElement:(NSString *)elementName 
+  namespaceURI:(NSString *)namespaceURI 
+ qualifiedName:(NSString *)qName
 {
-    if (!inTag) return;
-    [parser abortParsing];
+    if (!inTag) return; // if not inTag, then return
+    [parser abortParsing];  // if inTag, then stop parsing - this is only necessary when using XML - because we're
+							// using JSON instead, I don't think we need this bit?
 }
 
-@synthesize targetTag;
-@synthesize tagValue;
+
 @end
 
 
-
-
-
-
+// Also contained here is the loginParser implementation - will this still be using XML?
+// If so, then don't need to change anything. If it will be using JSON instead, then just use
+// same parsing method from above in findContentsOfTag method
 @implementation GZLoginParser
 
 - (NSString*)getSessionID:(NSData*) data
@@ -87,11 +100,10 @@
 	inSession=NO;
 //	NSString * xmlstring = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
 //	NSLog(@"Parsing:\n\n%@\n\n",xmlstring);
-    SBJsonParser *parser = [[SBJsonParser alloc] initWithData:data];
+    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
     // Set self as the delegate of the parser so that it will receive the parser delegate methods callbacks.
     [parser setDelegate:self];
     // Depending on the XML document you're parsing, you may want to enable these features of NSXMLParser.
-	// ^ no longer parsing XML, parsing Json instead - Liam
     [parser setShouldProcessNamespaces:NO];
     [parser setShouldReportNamespacePrefixes:NO];
     [parser setShouldResolveExternalEntities:NO];
@@ -101,13 +113,18 @@
 	return sessionID;
 }
 
-- (void)parser:(SBJsonParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict {
+- (void) parser:(NSXMLParser *)parser 
+didStartElement:(NSString *)elementName 
+   namespaceURI:(NSString *)namespaceURI 
+  qualifiedName:(NSString *)qName 
+	 attributes:(NSDictionary *)attributeDict 
+{
 	if (![elementName isEqualToString:@"input"]) return;
 	NSString * name=[attributeDict objectForKey:@"name"];
 	if (![name isEqualToString:@"lt"]) return;
 	sessionID = [NSString stringWithString:[attributeDict objectForKey:@"value"]];
 	[sessionID retain];
-	[parser abortParsing];
+	[parser abortParsing]; 
 	
 }
 
